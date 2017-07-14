2399d4b8584eb75815e4cd575fba9d5ea721e600
Sergi Martinez <sergimrtnz@gmail.com>
2017-07-14 09:48:31 +0200

    Splash Stuff

---
+++ app/src/main/java/com/sergi/motivapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.sergi.motivapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+
+    }
+
+}
